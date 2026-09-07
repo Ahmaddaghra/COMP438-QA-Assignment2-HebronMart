9600abc31133f1318cd1611fa07a79bb34b8555c
ahmad daghra <ahmaddaghra513@gmail.com>
2026-09-08 02:39:15 +0300

    test: add Ahmad data-driven search coverage

---
+++ Object Repository/Search/lbl_NoProducts.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Empty search-results message inside the stable product results container.</description>
+   <name>lbl_NoProducts</name>
+   <tag></tag>
+   <elementGuidId>a1757c73-860d-4b29-acb1-f87f3b76ee0c</elementGuidId>
+   <origin>STUDIOASSIST</origin>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#productListResultContainer .no-records-found</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
